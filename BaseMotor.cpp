041868b003dcaa4f67698fa4e1e3f52a686c9297
rodrig20 <rodrigo.f.lopes2004@gmail.com>
2025-06-17 20:05:10 +0100

    Fix alignment issues in BaseMotor

--- BaseMotor.cpp
+++ BaseMotor.cpp
@@ -7,20 +7,28 @@ BaseMotor::BaseMotor(Adafruit_PWMServoDriver* pwm, int motor_id)
 
 void BaseMotor::turn_90(int clockwise) {
     if (clockwise) {
-        turn_to(81);
+        turn_to(77+90);
     } else {
-        turn_to(0);
+        turn_to(-22+90);
+    }
+}
+
+void BaseMotor::turn_90_aligned(int clockwise) {
+    if (clockwise) {
+        turn_to(67+90);
+    } else {
+        turn_to(-14+90);
     }
 }
 
 void BaseMotor::turn_180(int clockwise) {
     if (clockwise) {
-        turn_to(168);
+        turn_to(154+90);
     } else {
-        turn_to(0);
+        turn_to(-14+90);
     }
 }
 
 void BaseMotor::to_default() {
-    turn_to(0);
+    turn_to(-14+90);
 }
